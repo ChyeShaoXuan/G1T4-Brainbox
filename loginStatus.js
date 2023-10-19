a8a5870a47db29fae9061038e99dbb661af8b722
ZimingSMU <ziming.wu.2022@scis.smu.edu.sg>
2023-10-20 02:19:32 +0800

    Updated home.html, index.html, Added home.js, loginStatus.js
    home.html: added user authentication check; added logout functionality
    home.js: logout functionality
    index.html: fixed typo
    loginStatus.js: Universal user authentication state check

---
+++ loginStatus.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js"
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,signOut} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEjW5Rq4jgHbSS1GCJy0pl6hpPrFQ9pUI",
+    authDomain: "wad2-4fc9e.firebaseapp.com",
+    projectId: "wad2-4fc9e",
+    storageBucket: "wad2-4fc9e.appspot.com",
+    messagingSenderId: "83590678050",
+    appId: "1:83590678050:web:6c98cdb011612eb4f5ac7b",
+    measurementId: "G-NY0ZMNMQLZ",
+    databaseURL: "https://wad2-4fc9e-default-rtdb.asia-southeast1.firebasedatabase.app"
+    };
+  
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+// const monitorAuthState = async() => {
+    
+// }
+
+function monitorAuthState() {
+    onAuthStateChanged(auth, user => {
+        if (user) {
+            console.log('user logged in')
+            console.log(user.uid)
+            document.getElementsByTagName('body')[0].removeAttribute('style')
+        } else {
+            window.location.replace("/Testing/Login/index.html")
+        }
+    })
+}
+
+
+
+window.addEventListener('load',monitorAuthState())
+
