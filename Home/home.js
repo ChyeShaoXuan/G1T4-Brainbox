11e806ed7780b1ae7c2a54666cabad1c947e5eaf
Koh Renyi <kohrenyi0802@gmail.com>
2023-10-21 14:34:12 +0800

    Update home.js

--- Home/home.js
+++ Home/home.js
@@ -20,11 +20,18 @@ const db = getDatabase()
 onAuthStateChanged(auth, user => {
         if (user) {
             const reference = ref(db,'users/' + user.uid + '/username')
-            onValue(reference, (snapshot) => {
-                document.getElementById('username').innerText = snapshot.val()
-            })
-        }
-    })
+    //         onValue(reference, (snapshot) => {
+    //             document.getElementById('username').innerText = snapshot.val()
+    //         })
+    //     }
+    // })
+    onValue(reference, (snapshot) => {
+    const usernameSpan = document.querySelector('#typed-text #username');
+    if (usernameSpan) {
+        usernameSpan.innerText = snapshot.val();
+    }
+});
+
 
 // For typing out animation
 const textElement = document.getElementById('typed-text');
