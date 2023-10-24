61ed07f9657f6f52b7ea765e03379a45ab361273
ZimingSMU <ziming.wu.2022@scis.smu.edu.sg>
2023-10-25 01:07:57 +0800

    Updated progress page
    Added Name

---
+++ Progress/progress.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js"
+import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEjW5Rq4jgHbSS1GCJy0pl6hpPrFQ9pUI",
+    authDomain: "wad2-4fc9e.firebaseapp.com",
+    projectId: "wad2-4fc9e",
+    storageBucket: "wad2-4fc9e.appspot.com",
+    messagingSenderId: "83590678050",
+    appId: "1:83590678050:web:6c98cdb011612eb4f5ac7b",
+    measurementId: "G-NY0ZMNMQLZ",
+    databaseURL: "https://wad2-4fc9e-default-rtdb.asia-southeast1.firebasedatabase.app"
+    };
+  
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getDatabase()
+
+function insertName() {
+    onAuthStateChanged(auth, user => {
+        if (user) {
+            const userRef = ref(db, 'users/'+ user.uid)
+            onValue(userRef, (snapshot) => {
+                document.getElementById('name').innerText = snapshot.val().username
+            })
+        } else {
+            window.location.replace("../Login/index.html")
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded',insertName)
