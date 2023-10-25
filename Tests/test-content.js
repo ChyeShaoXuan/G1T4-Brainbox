9b4058e71840b04cfbb71865b60002b2b0641fe2
Wesley Tan <wesleytan2206@gmail.com>
2023-10-25 22:24:48 +0800

    updated test content

--- Tests/test-content.js
+++ Tests/test-content.js
@@ -12,7 +12,10 @@ const app = Vue.createApp({
         axios.get('questions.json')
             .then((response) => {
                 this.questions = response.data;
-                console.log(this.questions[testSub][testDif].question)
+                this.questions[testSub][testDif].forEach(element => {
+                    console.log(element.question);
+                });
+                
             })
             .catch((error) => {
                 console.error('Error loading questions:', error);
