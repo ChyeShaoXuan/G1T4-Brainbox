782b6bed536b66db24719950311eaae30fd632b4
ZimingSMU <ziming.wu.2022@scis.smu.edu.sg>
2023-10-25 23:38:48 +0800

    Update forum.js

--- forum_page/forum.js
+++ forum_page/forum.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase()
-const postsRef = ref(db, 'posts/english');
+let postsRef = ref(db, 'posts/english');
 let postsUL = document.getElementById('posts')
 let newStr = ''
 let postsList = []
@@ -25,8 +25,7 @@ const userRef = ref(db,'users')
 const currSub = 'english'
 let currPost = null
 let currPage = 2
-
-document.addEventListener('DOMContentLoaded', () => {
+function showSearch() {
     let postsLength = 0
     onValue(postsRef,(snapshot) => {
         newStr = `<nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
@@ -46,12 +45,12 @@ document.addEventListener('DOMContentLoaded', () => {
         let noOfPages = Math.ceil(postsLength/5)
         for (let i=1;i<=noOfPages;i++) {
             if (i==1) {
-                newStr += `<a href="#" id='p1' aria-current="page" class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 page">1</a>`
+                newStr += `<a id='p1' aria-current="page" class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 page">1</a>`
             } else {
-                newStr += `<a href="#" id='p${i}' class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 page">${i}</a>`
+                newStr += `<a id='p${i}' class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 page">${i}</a>`
             }
         }
-        newStr += `<a href="#" class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+        newStr += `<a class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
         <span class="sr-only">Next</span>
         <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
           <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
@@ -69,17 +68,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }, {
         onlyOnce: true
     })
-
-})
-
+}
 
 function displayPage(page) {
     postsList = []
     let positionStr = ''
     let newStr = ''
-    document.getElementById('p'+page).setAttribute('class','relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 page')
-    document.getElementById('p'+currPage).setAttribute('class','relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 page')
-    currPage = page
     onValue(userRef, (snapshot) => {
         const users = snapshot.val()
         onValue(postsRef,(snapshot2) => {
@@ -89,45 +83,53 @@ function displayPage(page) {
                 uid = postDetails.uid
                 postsList.push([childKey,postDetails,users[postDetails.uid].image,users[postDetails.uid].username])
             });
-            postsList = postsList.reverse()
-            let firstPage = (page-1)*5
-            let lastPage = page*5
-            for (let i=firstPage;i<lastPage;i++) {
-                if (i >= postsList.length) {
+            if (postsList.length != 0) {
+                postsList = postsList.reverse()
+                let firstPage = (page-1)*5
+                let lastPage = page*5
+                for (let i=firstPage;i<lastPage;i++) {
+                    if (i >= postsList.length) {
+                        document.getElementById('firstPage').innerText = firstPage+1
+                        document.getElementById('lastPage').innerText = `${i}`
+                        break;
+                    }
                     document.getElementById('firstPage').innerText = firstPage+1
-                    document.getElementById('lastPage').innerText = `${i}`
-                    break;
-                }
-                document.getElementById('firstPage').innerText = firstPage+1
-                document.getElementById('lastPage').innerText = lastPage
-                currPost = postsList[i]
-                newStr+= `
-                    <li class="list-group-item p-3">
-                                            <div class="flex flex-col items-center md:flex-row">
-                                                <div class="w-full md:w-1/2 md:mb-0 text-center">
-                                                    <a href="post.html?postID=${currPost[0]}&subject=${currSub}" class="text-blue-900 font-semibold hover:text-red-900">${currPost[1].title}</a>
-                                                </div>
-                                                <div class="w-1/2 md:w-1/8 md:w-1/4">
-                                                    <div class="flex items-center">
-                                                        <div class="w-1/2">
-                                                            <img src="../Images/${currPost[2]}" class="w-10 h-10 rounded-full">
-                                                        </div>
-                                                        <div class="w-1/2">
-                                                            <span style="font-size: 15px;">${currPost[3]}</span>
+                    document.getElementById('lastPage').innerText = lastPage
+                    currPost = postsList[i]
+                    newStr+= `
+                        <li class="list-group-item p-3">
+                                                <div class="flex flex-col items-center md:flex-row">
+                                                    <div class="w-full md:w-1/2 md:mb-0 text-center">
+                                                        <a href="post.html?postID=${currPost[0]}&subject=${currSub}" class="text-blue-900 font-semibold hover:text-red-900">${currPost[1].title}</a>
+                                                    </div>
+                                                    <div class="w-1/2 md:w-1/8 md:w-1/4">
+                                                        <div class="flex items-center">
+                                                            <div class="w-1/2">
+                                                                <img src="../Images/${currPost[2]}" class="w-10 h-10 rounded-full">
+                                                            </div>
+                                                            <div class="w-1/2">
+                                                                <span style="font-size: 15px;">${currPost[3]}</span>
+                                                            </div>
                                                         </div>
                                                     </div>
+                                                    <div style="margin-top:1px;" class="w-1/2 md:w-1/8 md:w-1/4 md:mt-0">
+                                                        Replies: ${currPost[1].comments}
+                                                    </div>
+                                                    <div style="margin-top:1px;" class="w-1/2 md:w-1/8 md:w-1/4 md:mt-0">
+                                                        Views: ${currPost[1].views}
+                                                    </div>
                                                 </div>
-                                                <div style="margin-top:1px;" class="w-1/2 md:w-1/8 md:w-1/4 md:mt-0">
-                                                    Replies: ${currPost[1].comments}
-                                                </div>
-                                                <div style="margin-top:1px;" class="w-1/2 md:w-1/8 md:w-1/4 md:mt-0">
-                                                    Views: ${currPost[1].views}
-                                                </div>
-                                            </div>
-                                        </li>`
+                                            </li>`
+                }
+                document.getElementById('posts').innerHTML = newStr
+                document.getElementById('p'+page).setAttribute('class','relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 page')
+                document.getElementById('p'+currPage).setAttribute('class','relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 page')
+                currPage = page
+            } else {
+                document.getElementById('posts').innerHTML = ''
+                document.getElementById('firstPage').innerText = 0
+                document.getElementById('lastPage').innerText = 0
             }
-            document.getElementById('posts').innerHTML = newStr
-
         },{
             onlyOnce:true
         });
@@ -141,6 +143,28 @@ document.addEventListener('DOMContentLoaded', () => {
     displayPage(1)
 })
 
+document.addEventListener('DOMContentLoaded', showSearch)
+
+document.getElementById('english').addEventListener('click', () => {
+    postsRef = ref(db, 'posts/english');
+    currPage = 2
+    showSearch()
+    displayPage(1)
+})
+
+document.getElementById('math').addEventListener('click', () => {
+    postsRef = ref(db, 'posts/math');
+    currPage = 2
+    showSearch()
+    displayPage(1)
+})
+
+document.getElementById('science').addEventListener('click', () => {
+    postsRef = ref(db, 'posts/science');
+    currPage = 2
+    showSearch()
+    displayPage(1)
+})
 
 document.getElementById('create').addEventListener('click', function() {
     window.location.href = 'create.html';
