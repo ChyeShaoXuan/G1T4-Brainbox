ac1f46f797b524fc2546b25a6824f17131750af6
Wesley Tan <142380414+milkbaum@users.noreply.github.com>
2023-10-25 21:15:33 +0800

    Update test-content.js

--- Tests/test-content.js
+++ Tests/test-content.js
@@ -8,7 +8,7 @@ const app = Vue.createApp({
         axios.get('questions.json')
             .then((response) => {
                 this.questions = response.data;
-                console.log(questions)
+                console.log(this.questions)
             })
             .catch((error) => {
                 console.error('Error loading questions:', error);
