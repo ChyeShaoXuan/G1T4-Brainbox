07dc995b99bff2a397515c9a645fd29142960f77
Chye Shao Xuan <142379429+ChyeShaoXuan@users.noreply.github.com>
2023-10-27 20:46:32 +0800

    Update study.js

--- Study/study.js
+++ Study/study.js
@@ -6,6 +6,7 @@ function display_default(){
     axios.get(api_endpoint_url)
     .then(response => {  
       let default_data_arr=(response.data.english.grammar)
+      console.log(response.data.english);
       new_html=``;
     for (let i=0; i<default_data_arr.length;i++){
       let image=default_data_arr[i].image_url;
