a05c9c5e4341ba582571b8141be7d9ef26a5aefd
Chye Shao Xuan <142379429+ChyeShaoXuan@users.noreply.github.com>
2023-10-27 18:14:50 +0800

    Update study.js

--- Study/study.js
+++ Study/study.js
@@ -16,15 +16,15 @@ function display_default(){
              <img src="${image}" style="max-width:100%;
              max-height:100%;">
           </div>
-          <div class='col-sm-5 text-center rounded  ms-0 hover:scale-125' style='transition: transform .2s;background-image:url("../Images/study_grammarbackground.jpg");background-size:cover;background-repeat: no-repeat;'>
-               <p class="text-red-900 font-semibold text-md text-align-center m-5">${text}</p>
-               <p class="text-gray-400 font-semibold text-md text-align-center">${examples}</p>
+          <div class='col-sm-5 text-center rounded  ms-0 hover:scale-125' style='transition: transform .2s;background-image:url("../Images/study_content_frame.jpg");background-size:cover;background-repeat: no-repeat;'>
+               <p class="text-yellow-400 font-semibold text-md text-align-center m-5">${text}</p>
+               <p class="text-orange-500 font-semibold text-md text-align-center m-5">${examples}</p>
                 
           </div>
 
      </div>`
     }
-    let contents= document.getElementById('contents')
+    let contents= document.getElementById('notes')
     contents.innerHTML=new_html
       
 
@@ -38,13 +38,19 @@ function display_default(){
 }
 
 
-function populate_page(subject,topic){
+function populate_page(){
   let api_endpoint_url = "study.json"
 
     axios.get(api_endpoint_url)
     .then(response => {  
-      let data_arr=(response.data[`${subject}`][`${topic}`])
+      let dropdown=document.getElementById('dropdown_topics');
+      let value=dropdown.value;
       
+      let string_arr= value.split("-");
+      let subject=string_arr[0];
+      let topic=string_arr[1];
+      let data_arr=(response.data[`${subject}`][`${topic}`])
+      console.log(data_arr)
       new_html=``;
     for (let i=0; i<data_arr.length;i++){
       let image=data_arr[i].image_url;
@@ -55,17 +61,17 @@ function populate_page(subject,topic){
       <div class='col-sm-1'></div>
       <div class='col-sm-5 hover:scale-125' style='transition: transform .2s'>
          <img src="${image}" style="max-width:100%;
-         max-height:100%;">
+         max-height:100%;z-index:-1">
       </div>
-      <div class='col-sm-5 text-center rounded  ms-0 hover:scale-125' style='transition: transform .2s;background-image:url("../Images/study_grammarbackground.jpg");background-size:cover;background-repeat: no-repeat;'>
-           <p class="text-red-900 font-semibold text-md text-align-center m-5">${text}</p>
-           <p class="text-gray-400 font-semibold text-md text-align-center">${examples}</p>
+      <div class='col-sm-5 text-center rounded  ms-0 hover:scale-125' style='transition: transform .2s;background-image:url("../Images/study_content_frame.jpg");background-size:cover;background-repeat: no-repeat;'>
+           <p class="text-yellow-400 font-semibold text-md  text-align-center m-5">${text}</p>
+           <p class="text-orange-500 font-semibold text-md text-align-center m-5">${examples}</p>
             
       </div>
 
  </div>`
     }
-    let contents= document.getElementById('contents')
+    let contents= document.getElementById('notes')
     contents.innerHTML=new_html
     
   
