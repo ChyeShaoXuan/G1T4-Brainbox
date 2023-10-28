d12876a56050da46c065b94e1afeb43339108f01
milkbaum <142380414+milkbaum@users.noreply.github.com>
2023-10-28 22:59:31 +0800

    oof

--- Tests/test-content.js
+++ Tests/test-content.js
@@ -17,9 +17,10 @@ const app = Vue.createApp({
         axios.get('questions.json')
             .then((response) => {
                 this.questions= response.data[testSub][testDif]
+                console.log(this.questions);
 
                 // random 10 qns
-                for (let i=0;i<10;i++){
+                for (let i=0;i<5;i++){
                     let radnum=Math.floor(Math.random()*(this.questions.length))
                     let spliced_qns=this.questions.splice(radnum,1)[0]
                     spliced_qns.number=i+1
@@ -43,32 +44,53 @@ const app = Vue.createApp({
             var rads, test; // need to be set after load
             test = document.getElementById("test");
             rads = test.querySelectorAll("input[type=radio]"); // all radios in the quiz
+<<<<<<< Updated upstream
             //this.$refs.test.submit()
+=======
+            checked_rads=[] // radios user checked
+            
+>>>>>>> Stashed changes
             let counter = 0
-            for (var i=0;i<rads.length;i++) { // loop over all radios in the form
-                var rad = rads[i];
 
-                if (rad.checked) {
-                    console.log(rad.value,this.answers[counter])
-                    var correct = (rad.value==this.answers[counter]);
-                    console.log(correct)
-                    
-                    if (correct) {
+            for (var i=0;i<rads.length;i++){
+                var rad=rads[i]
+                if(rad.checked){
+                    checked_rads.push(rad.value)
+                }
+            }
+            console.log(checked_rads)
+            if (checked_rads.length==this.answers.length){
+                for (var i=0;i<rads.length;i++){
+                    var rad=rads[i]
+                    if (rad.checked){
+                        var correct = (rad.value==this.answers[counter])
+
+                    if (correct){
                         rad.closest("label").classList.toggle("correct");
-                        this.score +=1;
-                    }  
+                        this.score+=1
+                    }
                     else{
-                        rad.closest("label").classList.toggle("error")
-                    }  
-                    //console.log(counter);
+                        rad.closest("label").classList.toggle('error')
+                    }
                     counter++
                 }
-
+                }
+                
+                document.getElementById('submit').style.display='none'
+                document.getElementById('mark').style.display='block'
             }
+<<<<<<< Updated upstream
             document.getElementById('mark').style.display='block'
             document.getElementById('submit').style.display='none'
             this.show=true
             //document.getElementById("mark").setAttribute('value',score)
+=======
+            else{
+                alert
+            }
+                 
+                
+>>>>>>> Stashed changes
     },
         
 
