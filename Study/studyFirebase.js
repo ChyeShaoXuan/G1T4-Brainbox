83ea1781145123180a00aa8e80c39b5242bbbe60
ZimingSMU <ziming.wu.2022@scis.smu.edu.sg>
2023-11-01 08:56:14 +0800

    Updated study page

--- Study/studyFirebase.js
+++ Study/studyFirebase.js
@@ -17,10 +17,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase()
 const auth = getAuth(app)
+
 document.getElementById("completedButton").addEventListener("click", () => {
+    let value = document.getElementById("dropdown_topics").value
     onAuthStateChanged(auth, user => {
         const completeRef = ref(db, 'studyCompletion/' + user.uid)
-        let value = document.getElementById("dropdown_topics").value
         let updates = {}
         updates[value] = true;
         onValue(completeRef, (snapshot) => {
@@ -30,5 +31,31 @@ document.getElementById("completedButton").addEventListener("click", () => {
         }, {
             onlyOnce:true
         })
-    })  
+    })
+    disableConfirm()  
 })
+
+function disableConfirm() {
+    let value = document.getElementById("dropdown_topics").value
+    onAuthStateChanged(auth, user => {
+        const completeRef = ref(db,'studyCompletion/' + user.uid)
+        onValue(completeRef, (snapshot) => {
+            console.log(value,snapshot.val(),snapshot.val()[value])
+            if (snapshot.val()[value] != null) {
+                console.log("TEST")
+                document.getElementById('completedButton').setAttribute("class","mx-auto text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2")
+                document.getElementById("completedButton").disabled = true
+                document.getElementById("completedButton").innerText = "You have completed this module!"
+                
+            } else {
+                document.getElementById('completedButton').setAttribute("class","mx-auto text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2")
+                document.getElementById("completedButton").disabled = false
+                document.getElementById("completedButton").innerText = "I have completed everything!"
+            }
+        })
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded",disableConfirm)
+document.getElementById("dropdown_topics").addEventListener("change", disableConfirm)
